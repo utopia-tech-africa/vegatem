2cbba38263e915c95f3a6edd3fbf277245ba5b2a
efbrace <ebenezerflintwoodbrace@gmail.com>
2026-02-02 17:24:10 +0000

    add phone number

--- src/components/contact/components/connect-section.tsx
+++ src/components/contact/components/connect-section.tsx
@@ -22,6 +22,16 @@ const ConnectSection = () => {
           <Phone className="w-5 h-5" />
           <span>+233550713124</span>
         </div>
+        <a
+          href="tel:0593802668"
+          className="flex gap-2 items-center"
+          target="_blank"
+        >
+          <div className="flex items-center gap-3">
+            <Phone className="w-5 h-5" />
+            <span>+233593802668</span>
+          </div>
+        </a>
 
         <div className="flex gap-3">
           <MapPin className="w-5 h-5 mt-1" />
